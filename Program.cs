d0e132de09f2ebe0315e82b51ee2eb8f32ffaa07
Andrey <hliyena@yandex.ru>
2023-01-07 13:40:05 +0500

    add program data

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите массив данных через запятую: ");
+string strLine = Console.ReadLine();
+string[] ArrayA = strLine.Split(",");
+string[] ArrayB = new string[0];
+
+for (int i = 0; i < ArrayA.Length; i++)
+{
+    if (ArrayA[i].Length <= 3)
+    {
+        Array.Resize(ref ArrayB, ArrayB.Length + 1);
+        ArrayB[ArrayB.Length-1] = ArrayA[i];
+    }
+}
+Console.WriteLine($"[{string.Join(",", ArrayA)}]=>[{string.Join(",", ArrayB)}]");
